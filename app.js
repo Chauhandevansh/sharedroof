ebb11c40bb6c726c13eb038c4b146b0869a68a6e
Chauhandevansh <69097279+Chauhandevansh@users.noreply.github.com>
2021-12-28 13:34:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var fs = require('fs');
 var path = require('path');
 const bcrypt = require('bcryptjs')
 
-mongoose.connect("mongodb://localhost:27017/Room_Share", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://devansh:devdev30@cluster0.4g6il.mongodb.net/sharedroof?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connection Successfull...."))
     .catch((err) => console.log(err));
 
@@ -412,7 +412,7 @@ app.post("/login", async(req, res) => {
 
 
 
-var port = process.env.PORT || '3000'
+var port = process.env.PORT || '5000'
 app.listen(port, err => {
     if (err)
         throw err
